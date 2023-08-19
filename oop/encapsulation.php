c0bc6b5b6683c286e6fa45ea1c5eddf72f24c12a
Abu Motaleb <abumotaleb99@gmail.com>
2023-08-20 04:23:27 +0600

    implemented encapsulation by setting and getting employee salaries securely

---
+++ oop/encapsulation.php
@@ -0,0 +1,38 @@
+<?php
+class Employee {
+    private $name;
+    private $salary;
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+    
+    public function setSalary($salary) {
+        if ($salary > 0) {
+            $this->salary = $salary;
+        } else {
+            $message = "Invalid Salary!<br>";
+            echo $message;
+        }
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function getSalary() {
+        return $this->salary;
+    }
+}
+
+$employee = new Employee();
+
+$employee->setName("Abu Motaleb");
+$employee->setSalary(15000);
+
+echo "Employee Name: ".$employee->getName()."<br>";
+echo "Employee Salary: ".$employee->getSalary();
+
+?>
+
+
