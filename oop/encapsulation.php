2f8300e6b8415ca5908ea6a9a7b895f8ba1f6d70
Abu Motaleb <abumotaleb99@gmail.com>
2023-08-20 04:26:05 +0600

    updated encapsulation file

--- oop/encapsulation.php
+++ oop/encapsulation.php
@@ -6,7 +6,7 @@ class Employee {
     public function setName($name) {
         $this->name = $name;
     }
-    
+
     public function setSalary($salary) {
         if ($salary > 0) {
             $this->salary = $salary;
@@ -29,6 +29,12 @@ $employee = new Employee();
 
 $employee->setName("Abu Motaleb");
 $employee->setSalary(15000);
+
+echo "Employee Name: ".$employee->getName()."<br>";
+echo "Employee Salary: ".$employee->getSalary()."<br>";
+
+$employee->setName("Ashikur Rahman Khan");
+$employee->setSalary(20000);
 
 echo "Employee Name: ".$employee->getName()."<br>";
 echo "Employee Salary: ".$employee->getSalary();
